ca99a171f9c6f7e93d633b79ad53948af9b6bb15
Zito <irodgzito@gmail.com>
2026-06-27 23:04:20 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
+import os
+import psycopg2
 
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+def conectar_db():
+    return psycopg2.connect(
+        DATABASE_URL,
+        sslmode="require"
+    )
