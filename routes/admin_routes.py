279df094b96b9367c8a20473faa2000f606cce54
Zito <irodgzito@gmail.com>
2026-06-29 12:48:41 -0300

    Update admin_routes.py

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -7,4 +7,19 @@ from flask import (
     session
 )
 
+import cloudinary.uploader
+
+from services.auth import usuario_logado, usuario_admin
+
+from usuarios import (
+    buscar_usuario,
+    buscar_usuario_por_id,
+    listar_usuarios,
+    criar_usuario,
+    alterar_usuario,
+    alterar_senha,
+    alterar_foto,
+    excluir_usuario
+)
+
 admin_bp = Blueprint("admin", __name__)
