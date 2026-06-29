1e522e0f5cf728b0163048323ea75dbf42f09da1
Zito <irodgzito@gmail.com>
2026-06-29 12:25:20 -0300

    Update admin_routes.py

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session
+)
 
 admin_bp = Blueprint("admin", __name__)
