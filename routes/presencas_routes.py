deca1692b127223568b4deff2edc34b8e9646ff2
Zito <irodgzito@gmail.com>
2026-07-01 01:25:31 -0300

    Update presencas_routes.py

--- routes/presencas_routes.py
+++ routes/presencas_routes.py
@@ -3,7 +3,13 @@ from datetime import date
 
 from services.auth import usuario_logado
 from alunos import obter_alunos
-from presencas import obter_ou_criar_treino_do_dia, salvar_presencas
+from presencas import (
+    obter_ou_criar_treino_do_dia,
+    salvar_presencas,
+    listar_treinos,
+    listar_presentes_do_treino,
+    buscar_treino_por_id
+)
 
 
 presencas_bp = Blueprint("presencas", __name__)
@@ -28,4 +34,35 @@ def presencas():
         treino_id=treino_id,
         data_treino=hoje,
         alunos=alunos
+    )
+
+@presencas_bp.route("/presencas/historico")
+def historico_presencas():
+    if not usuario_logado():
+        return redirect(url_for("login.login"))
+
+    treinos = listar_treinos()
+
+    return render_template(
+        "historico_presencas.html",
+        treinos=treinos
+    )
+
+
+@presencas_bp.route("/presencas/<int:treino_id>")
+def ver_presenca(treino_id):
+    if not usuario_logado():
+        return redirect(url_for("login.login"))
+
+    treino = buscar_treino_por_id(treino_id)
+
+    if not treino:
+        return "Treino não encontrado.", 404
+
+    presentes = listar_presentes_do_treino(treino_id)
+
+    return render_template(
+        "ver_presenca.html",
+        treino=treino,
+        presentes=presentes
     )
